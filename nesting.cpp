607dce51079385e96c0934a3c60ec4378572abe9
Vishal <desaivishal4904@gmail.com>
2023-12-28 12:01:13 +0530

    Added .gitignore

---
+++ nesting.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<pair<int,int>> vp;
+    vp.push_back({1,2});
+    for (auto it : vp){
+        cout<<it.first<<" "<<it.second<<endl;
+    }
+}
