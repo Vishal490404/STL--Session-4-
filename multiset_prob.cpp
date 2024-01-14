0342a1976ad61c30231ea18f648712048b564747
Harshavardhan <harshabamane1718@gmail.com>
2024-01-14 12:53:01 +0530

    added testcases for  multiset question

--- multiset_prob.cpp
+++ multiset_prob.cpp
@@ -3,18 +3,39 @@
 using namespace std;
 
 int main(){
-    int n;
-	cin>>n;
-	multiset<int>ms;
-	for(int i=0;i<n;i++){
-		int temp;
-		cin>>temp;
-		ms.insert(temp);
-		for(auto& x:ms){
-			cout<<x<<" ";
-		}
-		cout<<"\n";
-	}
-    return 0;
+    // int n;
+	// cin>>n;
+	// multiset<int>ms;
+	// for(int i=0;i<n;i++){
+	// 	int temp;
+	// 	cin>>temp;
+	// 	ms.insert(temp);
+	// 	for(auto& x:ms){
+	// 		cout<<x<<" ";
+	// 	}
+	// 	cout<<"\n";
+	// }
+    // return 0;
+
+	testcase 1:
+	5
+	4 1 5 3 2
+
+	answer:
+	4
+	1 4
+	1 4 5
+	1 3 4 5
+	1 2 3 4 5
+
+	testcase 2:
+	4
+	4 4 3 2
+
+	answer:
+	4
+	4 4
+	3 4 4
+	2 3 4 4
 
 }
