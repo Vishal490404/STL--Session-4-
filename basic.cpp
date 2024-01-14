fb3d44c0fe26dc9ff125e7b7641092f9e1cefd3e
Vishal <desaivishal4904@gmail.com>
2024-01-14 09:07:20 +0530

    Final Commit

--- basic.cpp
+++ basic.cpp
@@ -1,7 +1,12 @@
 #include<bits/stdc++.h>
-
 using namespace std;
+void s(vector<int>&vec){
+    for (auto it : vec){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+}
 int main(){
     
-
+    return 0;
 }
--- iterators.cpp
+++ iterators.cpp
@@ -5,6 +5,5 @@ using namespace std;
 int main(){
     vector<int> v = {1,2,3,4,5,6};
     
-    
 
 }
